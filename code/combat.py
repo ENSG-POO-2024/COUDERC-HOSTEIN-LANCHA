a652622693d9ccecbeaeb384e820f08f8c468784
ellister29 <78070535+ellister29@users.noreply.github.com>
2024-05-03 17:08:18 +0200

    Update combat.py

--- code/combat.py
+++ code/combat.py
@@ -21,23 +21,33 @@ efficiencies = np.array([
     [0.5,2  ,1  ,1  ,1  ,0.5,1  ,1  ,2  ,1  ,2  ,1  ,1  ,0.5,1  ,1  ,1  ,1  ,1  ],
     [1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ]
     ])
-
+#On a défini la table des classes à l'aide d'un array numpy.
+#la 19e ligne/colonne correspond au "type" neutre qui est utile pour définir deux types ou pour l'attaque neutre
 types_dict = {"steel":0,"fighting":1,"dragon":2,"water":3,"electric":4,"fire":5,"fairy":6,"ice":7,"bug":8,"normal":9,"grass":10,"poison":11,"psychic":12,"rock":13,"ground":14,"ghost":15,"dark":16,"flying":17,"neutral":18}
+#l'utilisation d'un dictionnaire avec des indices correspondant au tableau permet une correspondance entre types et efficacités des attaques
 
 class Pokemon:
     def __init__(self,pv,atk,dfs,type1,type2 = "neutral"):
+        #On a défini par défaut le 2nd type comme neutre mais le programme accepte 2 types distincts
         self.type1 = type1
         self.type2 = type2
         self.pv = pv
         self.atk = atk
         self.dfs = dfs
+        #On a choisi d'ajouter des statistiques multiplicatives d'attaque et de défense qui permettent
+        #respectivement d'augmenter les dégâts infligés et de réduire les dégâts subis
         self.ko = False
     def attaque(self,type_atk,pui,adv):
+        #On utilise d'abord une fonction attaque dans le pokémon attaquant pour appliquer les éventuels modificateurs de dégâts liés au pokémon
         if self.type1 == type_atk or self.type2 == type_atk:
-            pui *= 1.5
+            #On a introduit le concept des attaques "STAB" : si le pokémon et son attaque sont de même type, celle-ci augmente de puissance
+            if not type_atk == "neutral":
+                #l'attaque neutre doit rester de puissance indépendante du type néanmoins
+                pui *= 1.5
         pui *= self.atk
+        #les dégâts augmentent avec la statistique d'attaque du pokémon
         adv.degats(type_atk,pui)
-        self.act = 0
+        #
     def degats(self,type_atk,pui):
         pui *= self.dfs
         pui *= efficiencies[types_dict[type_atk],types_dict[self.type1]]
@@ -45,4 +55,9 @@ class Pokemon:
             self.pv = 0
             self.ko = True
         else :
-            self.pv -= pui
+            self.pv = np.ceil(self.pv - pui)
+
+class combat:
+    def __init__(self,liste_pkmn,pkmn_sauvage):
+        pass
+    def tour_joueur(self):

1510de3728d3a25993b046d467adb3f9054a5afe
ellister29 <78070535+ellister29@users.noreply.github.com>
2024-05-06 12:11:31 +0200

    Update combat.py

--- code/combat/combat.py
+++ code/combat/combat.py
@@ -1,4 +1,13 @@
 import numpy as np
+from random import randint
+
+
+toutes_capacites = np.array([
+    ["charge"           ,1  ,"normal"   ,100],
+    ["fouet lianes"     ,1  ,"grass"    ,100],
+    ["écume"            ,1  ,"water"    ,100],
+    ["flammèche"        ,1  ,"fire"     ,100]
+    ])
 
 efficiencies = np.array([
     [0.5,1  ,1  ,0.5,0.5,0.5,2  ,2  ,1  ,1  ,1  ,1  ,1  ,2  ,1  ,1  ,1  ,1  ,1  ],
@@ -26,15 +35,46 @@ efficiencies = np.array([
 types_dict = {"steel":0,"fighting":1,"dragon":2,"water":3,"electric":4,"fire":5,"fairy":6,"ice":7,"bug":8,"normal":9,"grass":10,"poison":11,"psychic":12,"rock":13,"ground":14,"ghost":15,"dark":16,"flying":17,"neutral":18}
 #l'utilisation d'un dictionnaire avec des indices correspondant au tableau permet une correspondance entre types et efficacités des attaques
 
-def combat():
+def combat(liste_pkmn,pkmn_sauvage):
+    current_pokemon = liste_pkmn[0]
     combat_fini = False
+    fuite = False
+    capture = False
     while not combat_fini:
-        
-        pass
-    pass
+        a = [0,1] #on gerera ça plus tard avec qt
+        #a est du type [int,int], le premier entier donne l'action a realiser (attaque,changer de pokemon,capture,fuite)
+        #le second donne le detail de cette action (numero de l'attaque,du pokemon a changer)
+        if a[0] == 0:
+            print(0)
+            capacite = toutes_capacites[a[1]]
+            print("a")
+            print(capacite)
+            print("b")
+            current_pokemon.attaque(capacite,pkmn_sauvage)
+        elif a[0] == 1:
+            print(1)
+            #changer de pokemon
+            pass
+        elif a[0] == 2:
+            print(2)
+            capture = True
+        elif a[0] == 3:
+            print(3)
+            fuite = True
+        if pkmn_sauvage.ko or fuite or capture :
+            print(4)
+            combat_fini = True
+        else :
+            print(5)
+            pkmn_sauvage.attaque(pkmn_sauvage.liste_capacites[randint(0,1)],current_pokemon)
+        if current_pokemon.ko :
+            print(6)
+            combat_fini = True
+
+
 
 class Pokemon:
-    def __init__(self,pv,atk,dfs,type1,type2 = "neutral"):
+    def __init__(self,pv,atk,dfs,liste_capacites,type1,type2 = "neutral"):
         #On a défini par défaut le 2nd type comme neutre mais le programme accepte 2 types distincts
         self.type1 = type1
         self.type2 = type2
@@ -45,16 +85,20 @@ class Pokemon:
         #On a choisi d'ajouter des statistiques multiplicatives d'attaque et de défense qui permettent
         #respectivement d'augmenter les dégâts infligés et de réduire les dégâts subis
         self.ko = False
-    def attaque(self,type_atk,pui,adv):
+        self.liste_capacites = liste_capacites
+        
+    def attaque(self,capacite,adv):
         #On utilise d'abord une fonction attaque dans le pokémon attaquant pour appliquer les éventuels modificateurs de dégâts liés au pokémon
-        if self.type1 == type_atk or self.type2 == type_atk:
+        pui = capacite[1]
+        print(pui)
+        if self.type1 == capacite[2] or self.type2 == capacite[2]:
             #On a introduit le concept des attaques "STAB" : si le pokémon et son attaque sont de même type, celle-ci augmente de puissance
-            if not type_atk == "neutral":
+            if not capacite[2] == "neutral":
                 #l'attaque neutre doit rester de puissance indépendante du type néanmoins
                 pui *= 1.5
         pui *= self.atk
         #les dégâts augmentent avec la statistique d'attaque du pokémon
-        adv.degats(type_atk,pui)
+        adv.degats(capacite[2],pui)
     def degats(self,type_atk,pui):
         pui *= self.dfs
         #les dégâts sont réduits par la statistique de défense
@@ -73,16 +117,4 @@ class PokemonSauvage(Pokemon):
     #ceux-ci disposent en plus d'un taux de capture, et leur sprite est différent
     def __init__(self):
         super().__init__()
-
-class Combat:
-    def __init__(self,liste_pkmn,pkmn_sauvage):
-        self.liste_pkmn = liste_pkmn
-        self.pkmn_sauvage = pkmn_sauvage
-        self.is_tour_joueur = True
-        self.pokemon_actif = liste_pkmn[0]
-    
-    
-    def tour_sauvage(self):
-        self.pkmn_sauvage.attaque("neutral",50,self.pokemon_actif)
-    
     

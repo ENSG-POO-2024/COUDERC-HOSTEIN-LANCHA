3bc9a974b0f1a2cf09d4ecd8a2883235c8e3ba16
ellister29 <78070535+ellister29@users.noreply.github.com>
2024-05-15 18:24:26 +0200

    nettoyage

--- code/combat.py
+++ code/combat.py
@@ -9,35 +9,43 @@ def combat(liste_pkmn,pkmn_sauvage):
     fuite = False
     capture = False
     while not combat_fini:
-        a = [0,2] #on gerera ça plus tard avec qt
-        #a est du type [int,int], le premier entier donne l'action a realiser (attaque,changer de pokemon,capture,fuite)
-        #le second donne le detail de cette action (numero de l'attaque,du pokemon a changer)
+        #il faut un input de a
+        #c'est une liste de deux int
+        #le premier donne la nature de l'action, le deuxieme est specifique a chaque action
         if a[0] == 0:
+            #0 indique d'attquer
             print(0)
             capacite = current_pokemon.liste_capacites[a[1]]
+            #le deuxieme int donne quelle attaque choisir (0 à 3)
             print("a")
             print(capacite)
             print("b")
             current_pokemon.attaque(capacite,pkmn_sauvage)
         elif a[0] == 1:
+            #1 indique de changer de pokemon
+            #le deuxieme int donne le pokemon a echanger (1 à 5)
             print(1)
             #changer de pokemon
             pass
         elif a[0] == 2:
+            #2 indique une tentative de capture (le deuxieme int n'intervient pas)
             print(2)
             capture = True
         elif a[0] == 3:
+            #3 indique une tentative de fuite (le deuxieme int n'intervient pas)
             print(3)
             fuite = True
+        if current_pokemon.ko :
+            #si le pokemon actuel est ko, on change de pokemon, ou on tente une fuite
+            print(6)
+            combat_fini = True
         if pkmn_sauvage.ko or fuite or capture :
+            #on verifie si le combat est fini
             print(4)
             combat_fini = True
         else :
             print(5)
             pkmn_sauvage.attaque(pkmn_sauvage.liste_capacites[randint(0,1)],current_pokemon)
-        if current_pokemon.ko :
-            print(6)
-            combat_fini = True
 
 
 

375a33e7539c2c7a49dac65855502a68265c0011
ellister29 <78070535+ellister29@users.noreply.github.com>
2024-05-15 19:37:03 +0200

    combat
    les attaques fonctionnent youhou

--- code/combat.py
+++ code/combat.py
@@ -12,40 +12,31 @@ def combat(liste_pkmn,pkmn_sauvage):
         #il faut un input de a
         #c'est une liste de deux int
         #le premier donne la nature de l'action, le deuxieme est specifique a chaque action
+        a = [0,randint(0,3)]
         if a[0] == 0:
             #0 indique d'attquer
-            print(0)
             capacite = current_pokemon.liste_capacites[a[1]]
             #le deuxieme int donne quelle attaque choisir (0 à 3)
-            print("a")
-            print(capacite)
-            print("b")
             current_pokemon.attaque(capacite,pkmn_sauvage)
         elif a[0] == 1:
             #1 indique de changer de pokemon
             #le deuxieme int donne le pokemon a echanger (1 à 5)
-            print(1)
             #changer de pokemon
             pass
         elif a[0] == 2:
             #2 indique une tentative de capture (le deuxieme int n'intervient pas)
-            print(2)
             capture = True
         elif a[0] == 3:
             #3 indique une tentative de fuite (le deuxieme int n'intervient pas)
-            print(3)
             fuite = True
         if current_pokemon.ko :
             #si le pokemon actuel est ko, on change de pokemon, ou on tente une fuite
-            print(6)
             combat_fini = True
         if pkmn_sauvage.ko or fuite or capture :
             #on verifie si le combat est fini
-            print(4)
             combat_fini = True
         else :
-            print(5)
-            pkmn_sauvage.attaque(pkmn_sauvage.liste_capacites[randint(0,1)],current_pokemon)
+            pkmn_sauvage.attaque(pkmn_sauvage.liste_capacites[randint(0,3)],current_pokemon)
 
 
 
--- code/pokemon.py
+++ code/pokemon.py
@@ -7,6 +7,32 @@ from random import randint
 
 capacites = pd.read_csv("../data/abilities.csv")
 all_pokemon = pd.read_csv("../data/pokemon_first_gen.csv")
+all_pokemon["Type1"] = all_pokemon["Type1"].str.lower()
+all_pokemon["Type2"] = all_pokemon["Type2"].str.lower()
+
+def number_to_pokemon(num):
+    lvl = 100
+    num -= 1
+    name = all_pokemon.loc[num]["Name"]
+    pv = all_pokemon.loc[num]["HP"]
+    atk = all_pokemon.loc[num]["Attack"]
+    atk_spe = all_pokemon.loc[num]["Sp. Atk"]
+    dfs = all_pokemon.loc[num]["Defense"]
+    dfs_spe = all_pokemon.loc[num]["Sp. Def"]
+    vit = all_pokemon.loc[num]["Speed"]
+    liste_capacites = [all_pokemon.loc[num]["Move1"],all_pokemon.loc[num]["Move2"],all_pokemon.loc[num]["Move3"],all_pokemon.loc[num]["Move4"]]
+    type1 = all_pokemon.loc[num]["Type1"]
+    if not pd.isnull(all_pokemon.loc[num]["Type2"]) :
+        type2 = all_pokemon.loc[num]["Type2"]
+    else :
+        type2 = "neutral"
+    pv = np.floor(((2 * pv * lvl) / 100) + lvl + 10)
+    atk = np.floor(((2 * atk * lvl) / 100) + 5)
+    atk_spe = np.floor(((2 * atk_spe * lvl) / 100) + 5)
+    dfs = np.floor(((2 * dfs * lvl) / 100) + 5)
+    dfs_spe = np.floor(((2 * dfs_spe * lvl) / 100) + 5)
+    vit = np.floor(((2 * vit * lvl) / 100) + 5)
+    return Pokemon(name, pv, atk, atk_spe, dfs, dfs_spe, vit, liste_capacites, type1, type2)
 
 class Pokemon:
     def __init__(self,name,pv,atk,atk_spe,dfs,dfs_spe,vit,liste_capacites,type1,type2 = "neutral"):
@@ -27,26 +53,23 @@ class Pokemon:
         self.liste_capacites = liste_capacites
         #juste une liste de 4 int donnant l'indice de l'attaque
         self.iswild = False
-        self.lvl = pv / 4
+        self.lvl = 100
         #On n'utilise pas la mécanique de niveau, mais elle est nécessaire pr le calcul de dégats
-        #on fait donc une approximation via les pv
         
     def __str__(self):
         return self.name
     
     def attaque(self,num,adv):
+        print(num)
         type_atk = capacites["Type"][num]
-        dmg = 2 * self.lvl
-        dmg /= 5
-        dmg += 2
-        dmg *= capacites["Power"][num]
+        dmg = ((2 * self.lvl) / 5 + 2) * capacites["Power"][num]
         if capacites["Nature"][num] == "phy":
             dmg *= self.atk
             dmg /= adv.dfs
         elif capacites["Nature"][num] == "spe":
             dmg *= self.atk_spe
             dmg /= adv.dfs_spe
-        dmg +=2
+        dmg = (dmg / 50) + 2
         rnd = randint(217,255)
         rnd /= 255
         dmg *= rnd
@@ -58,16 +81,17 @@ class Pokemon:
         #les dégâts augmentent avec la statistique d'attaque du pokémon
         dmg *= types_pkmn.efficiencies[types_pkmn.dic[type_atk],types_pkmn.dic[adv.type1]]
         dmg *= types_pkmn.efficiencies[types_pkmn.dic[type_atk],types_pkmn.dic[adv.type2]]
-        adv.degats(type_atk)
-        print(self.name + " attaque " + capacites["Name"][num])
+        print(dmg)
+        adv.degats(dmg)
+        print(self.name + " utilise " + capacites["Name"][num])
         
-    def degats(self,type_atk,pui):
-        if pui >= self.pv:
+    def degats(self,dmg):
+        if dmg >= self.pv:
             self.pv = 0
             self.ko = True
             #Si l'attaque rend le pokemon KO, on met ses PV à 0 (pas de PV négatifs)
         else :
-            self.pv = np.ceil(self.pv - pui)
+            self.pv = np.ceil(self.pv - dmg)
 
 class PokemonSauvage(Pokemon):
     #On crée une sous-classe spécifiquement pour les pokémon sauvages
